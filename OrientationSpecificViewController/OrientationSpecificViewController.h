b55c5adf2398249fe1843fcbc73204f21a5c4fdf
Vinayak Ram <vinnybad@gmail.com>
2014-01-21 20:37:53 -0600

    Initial Commit

---
+++ OrientationSpecificViewController/OrientationSpecificViewController.h
@@ -0,0 +1,13 @@
+//
+//  OrientationSpecificViewController.h
+//  OrientationSpecificViewController
+//
+//  Created by Vinayak Ram on 1/21/14.
+//  Copyright (c) 2014 Vinayak Badrinathan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OrientationSpecificViewController : NSObject
+
+@end
---
+++ OrientationSpecificViewController/OrientationSpecificViewController.m
@@ -0,0 +1,13 @@
+//
+//  OrientationSpecificViewController.m
+//  OrientationSpecificViewController
+//
+//  Created by Vinayak Ram on 1/21/14.
+//  Copyright (c) 2014 Vinayak Badrinathan. All rights reserved.
+//
+
+#import "OrientationSpecificViewController.h"
+
+@implementation OrientationSpecificViewController
+
+@end
